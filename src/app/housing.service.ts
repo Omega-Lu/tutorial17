c42622c45a8904ba8a2255c2799e875eeaa16fb3
Thando Dlomo <ThandoD@uis-as.co.za>
2024-04-23 14:50:18 +0200

    Houses

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -6,7 +6,7 @@ import { HousingLocation } from './housinglocation';
 })
 export class HousingService {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
+  url = 'http://localhost:3000/locations';
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -109,12 +109,18 @@ export class HousingService {
       laundry: true
     }
   ];
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
   
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
   constructor() { }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
 }
